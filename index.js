b9a9a55dd014f456904cbb632864bb44cae044e9
Sufyan Hussein <sufianb.contact@gmail.com>
2023-01-29 01:55:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+document.createElement("div");
+document.body.append(element);
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++){
+    const li = document.createElement("li");
+    li.textContent =( i+1 ).toString();
+    ul.append(li);
+
+}
+element.append(ul)
+const main = document.getElementById("main")
+main.remove()
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "Sufyan is the champion"
+ 
